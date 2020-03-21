a47418c4c9f1e6bfe97f597e6e6b671b3420d84f
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2020-03-21 21:10:40 +0100

    not working with leaflet

--- components/consumer/map.js
+++ components/consumer/map.js
@@ -1,6 +1,31 @@
-const ConsumerMap = () => {
-  return (
-    <div id="map"></div>
-  );
+const { Map: LeafletMap, TileLayer, Marker, Popup } = require('react-leaflet');
+
+class ConsumerMap extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    }
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <LeafletMap center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
 }
+
 export default ConsumerMap;
